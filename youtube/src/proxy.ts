588b9b0675217b31450b115b0aa9addcad6386b8
Pasang Tamang <110292256+Websoulmate@users.noreply.github.com>
2026-05-04 02:54:44 +0300

    change middlewear to proxy

--- youtube/src/proxy.ts
+++ youtube/src/proxy.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
   const isPublicPath =
